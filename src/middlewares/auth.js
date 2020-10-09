6dc30ca5944f18a6aa5ac586ed228a76f3595fc1
tarikcarli <pro.tarikcarli@gmail.com>
2020-10-09 17:09:23 +0300

    reactoring auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,45 +1,22 @@
 const jwt = require("jsonwebtoken");
 const redis = require("../connections/redis");
+const { sequelize } = require("../connections/db");
 const { env } = require("../config/env");
 const response = require("../utilities/response");
 
 /**
  *
- * @param {import("express").Request} req
- * @param {import("express").Response} res
- * @param {import("express").NextFunction} next
- * @returns {undefined}
+ * @param {String} token
+ * @param {String} secret
  */
-function auth(req, res, next) {
-  const token = req.headers.authorization;
-  if (!token) {
-    const options = {
-      message: "Token is required.",
-      status: 400,
-    };
-    return response(options, req, res, next);
-  }
-
-  return jwt.verify(token, env.secret, async (err, decoded) => {
-    if (err) {
-      console.log(`Auth.jwt.verify Error ${err}`);
-      const options = {
-        message: "Token is invalid.",
-        status: 401,
-      };
-      return response(options, req, res, next);
-    }
-    req.reqUserId = Number(decoded.id);
-    req.reqUserRole = Number(decoded.role);
-    const reply = await redis.get(`token${req.reqUserId.toString()}`);
-    if (reply && reply === token) {
-      return next();
-    }
-    const options = {
-      message: "Token is invalid.",
-      status: 401,
-    };
-    return response(options, req, res, next);
+function verify(token, secret) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, secret, async (err, decoded) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(decoded);
+    });
   });
 }
 
@@ -50,11 +27,47 @@ function auth(req, res, next) {
  * @param {import("express").NextFunction} next
  * @returns {undefined}
  */
-function onlyCompany(req, res, next) {
-  const { reqUserRole } = req;
-  if (reqUserRole === 1) {
+async function auth(req, res, next) {
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      const options = {
+        message: "Token is required.",
+        status: 400,
+      };
+      return response(options, req, res, next);
+    }
+    const decoded = await verify(token, env.secret);
+    req.userId = Number(decoded.id);
+    req.userRole = Number(decoded.role);
+    req.db = sequelize.models;
+    const reply = await redis.get(`token${req.reqUserId.toString()}`);
+    if (!reply || reply !== token) {
+      throw new Error("Token isn't stored.");
+    }
+  } catch (err) {
+    console.log(`Auth Error ${err}`);
     const options = {
-      message: "You must be company.",
+      message: "Token is invalid.",
+      status: 401,
+    };
+    return response(options, req, res, next);
+  }
+  return next();
+}
+
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ * @returns {undefined}
+ */
+function onlyCompany(req, res, next) {
+  const { userRole } = req;
+  if (userRole === 1) {
+    const options = {
+      message: "You must have company role.",
       status: 401,
     };
     return response(options, req, res, next);
@@ -70,10 +83,10 @@ function onlyCompany(req, res, next) {
  * @returns {undefined}
  */
 function onlyWorker(req, res, next) {
-  const { reqUserRole } = req;
-  if (reqUserRole === 0) {
+  const { userRole } = req;
+  if (userRole === 0) {
     const options = {
-      message: "You must be worker.",
+      message: "You must have worker role.",
       status: 401,
     };
     return response(options, req, res, next);
